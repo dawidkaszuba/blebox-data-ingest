03fbd128d8b2f1a6c97e98c8f48e9b08104a9bd1
dawidkaszuba <d.kaszuba89@gmail.com>
2025-07-31 22:50:39 +0200

    first commit

---
+++ src/main/java/pl/dawidkaszuba/blebox_data_ingest/BleboxDataIngestApplication.java
@@ -0,0 +1,13 @@
+package pl.dawidkaszuba.blebox_data_ingest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BleboxDataIngestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BleboxDataIngestApplication.class, args);
+	}
+
+}
